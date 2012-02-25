90f4f593dba7cf11c674eeced7c88bb908a2e6d1
striking <chris@strikinggroup.com.au>
2012-02-25 14:32:39 +1000

    completed the layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @title = "home"
   end
 
   def contact
+  @title = "Contact us"
   end
-
+  
+  def about
+  @title = "About us"
+  end
 end
